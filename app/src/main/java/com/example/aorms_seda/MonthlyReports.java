f3d58bd17da12f1c1cc261e48ffc5789921aa957
Adil <l164335@lhr.nu.edu.pk>
2019-11-04 21:20:28 +0500

    initialization

---
+++ app/src/main/java/com/example/aorms_seda/MonthlyReports.java
@@ -0,0 +1,29 @@
+package com.example.aorms_seda;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MonthlyReports extends Fragment {
+
+    public MonthlyReports() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_monthly_reports, container, false);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/aorms_seda/SectionsPagerAdapter.java
@@ -0,0 +1,55 @@
+package com.example.aorms_seda;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.aorms_seda.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+}
