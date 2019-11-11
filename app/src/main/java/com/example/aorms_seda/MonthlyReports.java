9baef7f031cfc121144411faeb631b192367316c
Duraze007 <duraze460@gmail.com>
2019-11-11 19:33:54 +0500

    with recycleview and bar chart etc from Duraze Saquib

--- app/src/main/java/com/example/aorms_seda/MonthlyReports.java
+++ app/src/main/java/com/example/aorms_seda/MonthlyReports.java
@@ -9,6 +9,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.charts.Pie;
+import com.anychart.core.gauge.pointers.Bar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MonthlyReports extends Fragment {
@@ -21,8 +31,22 @@ public class MonthlyReports extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_monthly_reports, container, false);
+
+        View view=inflater.inflate(R.layout.fragment_monthly_reports, container, false);
+
+        Cartesian br = AnyChart.bar();
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("John", 10000));
+        data.add(new ValueDataEntry("Jake", 12000));
+        data.add(new ValueDataEntry("Peter", 18000));
+
+        br.data(data);
+
+        AnyChartView anyChartView = (AnyChartView) view.findViewById(R.id.any_chart_view);
+        anyChartView.setChart(br);
+
+        return view;
     }
 
 
